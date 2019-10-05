8a90aff09b97ea6b0acdba9fd576cb00a4de50ad
FredrickBarrett <f.barrett12@gmail.com>
2019-10-05 12:10:51 -0400

    Fixed a bug where new users were not directed to the page with all their blogs that they don't have yet lol

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
         if @user.valid?
             confirm_password
             session[:user_id] = @user.id
-            redirect_to user_path(@user)
+            redirect_to posts_path(@user)
         else
             flash[:errors] = @user.errors.full_messages
             redirect_to new_user_path
